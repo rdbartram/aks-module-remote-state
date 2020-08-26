c697ad6b873f0f2856f8ecffe2fb88e23e18d8c9
Ryan Bartram <ryan@bartram.ch>
2020-08-26 18:27:06 +0100

    test

--- 20-azure-remote-state.tf
+++ 20-azure-remote-state.tf
@@ -5,7 +5,7 @@
 data "terraform_remote_state" "remote_state" {
   backend = "azurerm"
 
-  config {
+  config = {
     storage_account_name = "${var.storage_account_name}"
     key                  = "${var.key}"
     container_name       = "${var.container_name}"
